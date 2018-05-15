abd4dd37bfb18f2d3134916c8548de07e5c970dc
Palamente <39123893+Palamente@users.noreply.github.com>
2018-05-15 16:28:31 +0200

    Add files via upload

---
+++ palamente1/palamente1/js/bruit_ancre.js
@@ -0,0 +1,14 @@
+$(document).ready(function() 
+{
+	$('.cRetour').click(function()
+	{
+		$('#ancre').get(0).play();
+	});
+
+	$('.ancre_partenaire').click(function()
+	{
+		$('#ancre').get(0).play();
+	});
+
+
+});
---
+++ palamente1/palamente1/projets/chemin_de_croisement.php
@@ -0,0 +1,52 @@
+<div class="col-md-12">
+	<div class="page-header">
+		<h1 class="titre">
+			Chemin de croisement
+		</h1>
+	</div>
+	<figure class="figure" style="display:inline">
+		<figcaption class="figure-caption" style="display:inline">Dossier</figcaption>
+		<a href="doc/chemin_de_croisement/dossier chemin de croisement - Mail.pdf"><img src="images/logodossier.jpg" class="figure-img img-fluid rounded"></a>
+		
+	</figure>
+
+	<img src="images/chemin_de_croisement/chemins de croisement 001.jpg">
+
+	<figure class="figure" style="display:inline">
+		<img id="jereve" style="cursor:pointer;" src="images/logoentendre.jpg" class="figure-img img-fluid rounded">
+		<figcaption class="figure-caption" style="display:inline;">"Je rêve"</figcaption>
+	</figure>
+</div>
+
+<div id="son_reve" class="col-md-12" style="display:none;">
+	<audio controls>
+		<source src="doc/chemin_de_croisement/jereve.mp3" type="audio/mpeg" />
+		<a href="doc/chemin_de_croisement/jereve.mp3">horse</a>
+	</audio>
+</div>
+
+<div class="col-md-12">
+	<dl>
+		<p>
+		<strong>Chemins de croisement</strong> est un projet entre transmission d’une technique à des enfants et mise en jeu de l’art du comédien. Son objectif est de rendre compte de la place de l'artiste dans son travail d'intervention en milieu scolaire.  
+		</p><br/>
+		<p>
+		<strong>Chemins de croisement</strong> propose un échange constant entre l'enseignant, l'enfant et l'artiste impliqué dans un travail de création : au même titre que dans sa pratique, l'enfant découvre le théâtre en regardant l'acteur jouer.
+		</p><br/>
+		<p>
+		Depuis 2004 le travail effectué à Epinay sur Seine (MTD) auprès du jeune public opère dans cette direction. Il donne lieu chaque année à des rencontres entre élèves de différentes écoles autour de petites formes théâtrales imaginées par les comédiens sur des textes d'auteurs contemporains. 
+		</p><br/>
+		<p>
+		<strong>Chemins de croisement</strong> se poursuit actuellement dans les écoles, les centres socioculturels, au théâtre avec deux comédiennes, Emmanuelle Danesi et Sabine Larivière. <strong>Coproduction : MTD Epinay sur Seine</strong>
+		</p><br/>
+		<p>L'initiative de ce projet a permis la création d'une pièce de théâtre, commande d'écriture à Jean Gabriel Nordmann qui a reçu l’aide à la création de la SACD- Fondation Beaumarchais. Avec trois comédiens professionnels et  quatre enfants, ayant participé aux ateliers de création menés à Epinay sur Seine.
+		</p><br/>
+		<p>
+			<strong>"Devant l'enfant"</strong> a été joué 40 fois durant la saison 2007-2008 à Epinay sur Seine, à Paris…
+			</p><br/>
+			<img src="images/chemin_de_croisement/devant enfant.jpg" class="img-fluid" alt="Responsive image">
+			<img src="images/chemin_de_croisement/bulles costumes demo.jpg" class="img-fluid" alt="Responsive image">
+			<img src="images/chemin_de_croisement/2 bulles.jpg" class="img-fluid" alt="Responsive image">
+			<img src="images/chemin_de_croisement/bulles costumes.jpg" class="img-fluid" alt="Responsive image">
+	</dl>
+</div>
