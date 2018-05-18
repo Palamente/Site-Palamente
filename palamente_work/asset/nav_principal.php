6b303641411ef5c4843a68cd3cb72a6eaa4310ae
MrPul <33284285+MrPul@users.noreply.github.com>
2018-05-18 10:46:14 +0200

    ménage

--- palamente_work/asset/nav_principal.php
+++ palamente_work/asset/nav_principal.php
@@ -14,18 +14,18 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
             <li class="animated fadeInLeft">
-                <a href="index.html">Actuellement <br/><br/><input type="image" class="btn-img" src="SVG/vague bas.svg"/></a>
+                <a href="index.html">Actuellement <br/><br/><input type="image" class="btn-img" src="images/SVG/vague bas.svg"/></a>
             <li class="animated fadeInLeft">
-              <a href="compagnie.html">La Compagnie <br/><br/><input type="image" class="btn-img" src="SVG/vague ht.svg"/></a>
+              <a href="compagnie.html">La Compagnie <br/><br/><input type="image" class="btn-img" src="images/SVG/vague ht.svg"/></a>
             </li>
             <li class="animated fadeInUp">
-                <a href="spectacle.html">Spectacles <br/><br/><input type="image" class="btn-img" src="SVG/vague bas.svg"/></a>
+                <a href="spectacle.html">Spectacles <br/><br/><input type="image" class="btn-img" src="images/SVG/vague bas.svg"/></a>
             </li>
             <li class="animated fadeInRight">
-                <a href="projet.html">Créations en cours <br/><br/><input type="image" class="btn-img" src="SVG/vague ht.svg"/></a>
+                <a href="projet.html">Créations en cours <br/><br/><input type="image" class="btn-img" src="images/SVG/vague ht.svg"/></a>
             </li>
             <li class="animated fadeInRight">
-                <a href="contact.html#ancre_nav">Contacts <br/><br/><input type="image" class="btn-img" src="SVG/vague bas.svg"/></a>
+                <a href="contact.html#ancre_nav">Contacts <br/><br/><input type="image" class="btn-img" src="images/SVG/vague bas.svg"/></a>
             </li>
         </ul>
     </div>
